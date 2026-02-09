ade5d24e7a4d7fb86fd439970bce50d4a031e9e6
Antonio <230642@utxicotepec.edu.mx>
2026-02-09 09:59:30 -0600

    Se agregaron archivos de la carpeta de schemas, models, y config.db

--- config/db.py
+++ config/db.py
@@ -2,7 +2,8 @@ from sqlalchemy create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root1234@127.0.0.1:3306/autolavado"    //o lavanderia//
+SQLALCHEMY_DATABASE_URL = "mysql://root1234@127.0.0.1:3306/sistema-de-lavanderia"    //o lavanderia//
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+Base = declarative_base()   
+
--- models/modeluser.py
+++ models/modeluser.py
@@ -1,10 +1,33 @@
 from sqlalchemy import column
 from sqlalchemy.orm
-fromc config
+#pylint: disable=import.error
+from config import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from datetime import datetime
+
 
 class User(Base):
-    __tablename__ = 'the_users'
+    __tablename__ = "the_users"
+
     id = Column(Integer, primary_key=True, index=True)
     rol_id = Column(Integer, ForeignKey('the_rols.id'))
+    nombre = Column(String(100), nullable=False)
+    primer_apellido = Column(String(100), nullable=False)
+    segundo_apellido = Column(String(100), nullable=False)
+    correo = Column(String(150), unique=True, nullable=False)
+    contraseña = Column(String(255), nullable=False)
+    telefono_celular = Column(String(20))
+
+
+
+
+    estatus = Column(Boolean, default=True)
+
+    fecha_registro = Column(DateTime, default=datetime.utcnow)
+    fecha_actualizacion = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    rol_id = Column(Integer, ForeignKey("the_roles.id"), nullable=False)
+
+    rol=relationship("Rol",back_populates="usuarios" )
 
     
